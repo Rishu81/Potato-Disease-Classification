354ce8a37caf5919a6de450e47c2b39b2728347c
unknown <Rishabhgupta7981@gmail.com>
2024-10-27 10:00:00 +0530

    Fixed the bug in recommendation algorithm

---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+import random
+
+rnd = random.uniform(0.95,1)
+app = FastAPI()
+
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+
+    predicted_class = CLASS_NAMES[0]
+    confidence = np.max(rnd*100)
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
+
